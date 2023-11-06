0a5f4e0aa1d9499adfea0364ac3a84caf7066864
smit5217 <89056600+smit5217@users.noreply.github.com>
2023-11-06 16:13:36 +0530

    admin sidebar-chart

--- lms/src/components/Dashboard/Admin/Dashboard.js
+++ lms/src/components/Dashboard/Admin/Dashboard.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import Chart from "chart.js/auto";
 import DarkNight from "../../DarkNight";
 import TopBar from "../../Topbar";
 import Navbar from "../../Navbar";
@@ -28,8 +29,104 @@ import verify from "../../../img/dashbord/verify.png";
 import video from "../../../img/dashbord/video.png";
 import DANavBar from "./DANavBar/DANavBar";
 import { Link } from "react-router-dom";
+import DASidebar from "./Sidebar/DASidebar";
 
 const AdminDashboard = () => {
+  const pieChartRef = useRef(null);
+  const lineChartRef = useRef(null);
+
+  useEffect(() => {
+    const pieChartCanvas = document.getElementById("pieChart");
+    if (!pieChartCanvas) return;
+
+    if (pieChartRef.current) {
+      pieChartRef.current.destroy();
+    }
+
+    pieChartRef.current = new Chart(pieChartCanvas, {
+      type: "pie",
+      data: {
+        labels: ["Direct", "Referral", "Organic"],
+        datasets: [
+          {
+            label: "#",
+            data: [40, 28, 32],
+          },
+        ],
+      },
+      options: {
+        cutout: "75%",
+        plugins: {
+          legend: {
+            position: "left",
+          },
+        },
+        elements: {
+          arc: {
+            backgroundColor: "#5F2DED",
+            hoverBackgroundColor: "#5F2DED",
+          },
+        },
+      },
+    });
+  }, []);
+
+  useEffect(() => {
+    const lineChartCanvas = document.getElementById("lineChart");
+    if (!lineChartCanvas) return;
+
+    if (lineChartRef.current) {
+      lineChartRef.current.destroy();
+    }
+
+    lineChartRef.current = new Chart(lineChartCanvas, {
+      type: "line",
+      data: {
+        labels: [
+          "Jan",
+          "Feb",
+          "Marc",
+          "April",
+          "May",
+          "Jun",
+          "July",
+          "Agust",
+          "Sept",
+          "Oct",
+          "Now",
+          "Dec",
+        ],
+        datasets: [
+          {
+            label: "#",
+            data: [148, 100, 205, 110, 165, 145, 180, 156, 148, 220, 180, 245],
+            tension: 0.4,
+            backgroundColor: "#5F2DED",
+            borderColor: "#5F2DED",
+            borderWidth: 2,
+          },
+        ],
+      },
+      options: {
+        responsive: true,
+        plugins: {
+          legend: {
+            display: false,
+          },
+        },
+        scales: {
+          y: {
+            min: 0,
+            max: 300,
+            ticks: {
+              stepSize: 50,
+            },
+          },
+        },
+      },
+    });
+  }, []);
+
   return (
     <>
       <DarkNight />
@@ -40,7 +137,7 @@ const AdminDashboard = () => {
           <div className="theme__shadow__circle"></div>
           <div className="theme__shadow__circle shadow__right"></div>
           <div className="dashboardarea sp_bottom_100">
-            <DANavBar/>
+            <DANavBar />
             <div className="dashboard">
               <div className="container-fluid full__width__padding">
                 <div className="row">
@@ -49,177 +146,7 @@ const AdminDashboard = () => {
                       <div className="dashboard__nav__title">
                         <h6>Welcome, Micle Obema</h6>
                       </div>
-                      <div className="dashboard__nav">
-                        <ul>
-                          <li>
-                            <Link className="active" to="/dashboard/admin-dashboard">
-                              <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                width="24"
-                                height="24"
-                                viewBox="0 0 24 24"
-                                fill="none"
-                                stroke="currentColor"
-                                strokeWidth="2"
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                className="feather feather-home"
-                              >
-                                <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
-                                <polyline points="9 22 9 12 15 12 15 22"></polyline>
-                              </svg>
-                              Dashboard
-                            </Link>
-                          </li>
-                          <li>
-                            <Link to="/dashboard/admin-profile">
-                              <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                width="24"
-                                height="24"
-                                viewBox="0 0 24 24"
-                                fill="none"
-                                stroke="currentColor"
-                                strokeWidth="2"
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                className="feather feather-user"
-                              >
-                                <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
-                                <circle cx="12" cy="7" r="4"></circle>
-                              </svg>
-                              My Profile
-                            </Link>
-                          </li>
-                          <li>
-                            <Link to="/dashboard/admin-message">
-                              <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                width="24"
-                                height="24"
-                                viewBox="0 0 24 24"
-                                fill="none"
-                                stroke="currentColor"
-                                strokeWidth="2"
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                className="feather feather-book-open"
-                              >
-                                <path d="M2 3h6a4 4 0 0 1 4 4v14a3 3 0 0 0-3-3H2z"></path>
-                                <path d="M22 3h-6a4 4 0 0 0-4 4v14a3 3 0 0 1 3-3h7z"></path>
-                              </svg>
-                              Message
-                            </Link>
-                            <span className="dashboard__label">12</span>
-                          </li>
-                          <li>
-                            <Link to="/dashboard/admin-message">
-                              <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                width="24"
-                                height="24"
-                                viewBox="0 0 24 24"
-                                fill="none"
-                                stroke="currentColor"
-                                strokeWidth="2"
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                className="feather feather-bookmark"
-                              >
-                                <path d="M19 21l-7-5-7 5V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z"></path>
-                              </svg>
-                              Courses
-                            </Link>
-                          </li>
-                          <li>
-                            <Link to="/dashboard/admin-review">
-                              <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                width="24"
-                                height="24"
-                                viewBox="0 0 24 24"
-                                fill="none"
-                                stroke="currentColor"
-                                strokeWidth="2"
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                className="feather feather-star"
-                              >
-                                <polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"></polygon>
-                              </svg>
-                              Reviews
-                            </Link>
-                          </li>
-                          <li>
-                            <Link to="/dashboard/admin-quiz">
-                              <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                width="24"
-                                height="24"
-                                viewBox="0 0 24 24"
-                                fill="none"
-                                stroke="currentColor"
-                                strokeWidth="2"
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                className="feather feather-help-circle"
-                              >
-                                <circle cx="12" cy="12" r="10"></circle>
-                                <path d="M9.09 9a3 3 0 0 1 5.83 1c0 2-3 3-3 3"></path>
-                                <line x1="12" y1="17" x2="12.01" y2="17"></line>
-                              </svg>
-                              Quiz Attempts
-                            </Link>
-                          </li>
-                        </ul>
-                      </div>
-                      <div className="dashboard__nav__title mt-40">
-                        <h6>user</h6>
-                      </div>
-                      <div className="dashboard__nav">
-                        <ul>
-                          <li>
-                            <Link to="/dashboard/admin-settings">
-                              <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                width="24"
-                                height="24"
-                                viewBox="0 0 24 24"
-                                fill="none"
-                                stroke="currentColor"
-                                strokeWidth="2"
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                className="feather feather-settings"
-                              >
-                                <circle cx="12" cy="12" r="3"></circle>
-                                <path d="M19.4 15a1.65 1.65 0 0 0 .33 1.82l.06.06a2 2 0 0 1 0 2.83 2 2 0 0 1-2.83 0l-.06-.06a1.65 1.65 0 0 0-1.82-.33 1.65 1.65 0 0 0-1 1.51V21a2 2 0 0 1-2 2 2 2 0 0 1-2-2v-.09A1.65 1.65 0 0 0 9 19.4a1.65 1.65 0 0 0-1.82.33l-.06.06a2 2 0 0 1-2.83 0 2 2 0 0 1 0-2.83l.06-.06a1.65 1.65 0 0 0 .33-1.82 1.65 1.65 0 0 0-1.51-1H3a2 2 0 0 1-2-2 2 2 0 0 1 2-2h.09A1.65 1.65 0 0 0 4.6 9a1.65 1.65 0 0 0-.33-1.82l-.06-.06a2 2 0 0 1 0-2.83 2 2 0 0 1 2.83 0l.06.06a1.65 1.65 0 0 0 1.82.33H9a1.65 1.65 0 0 0 1-1.51V3a2 2 0 0 1 2-2 2 2 0 0 1 2 2v.09a1.65 1.65 0 0 0 1 1.51 1.65 1.65 0 0 0 1.82-.33l.06-.06a2 2 0 0 1 2.83 0 2 2 0 0 1 0 2.83l-.06.06a1.65 1.65 0 0 0-.33 1.82V9a1.65 1.65 0 0 0 1.51 1H21a2 2 0 0 1 2 2 2 2 0 0 1-2 2h-.09a1.65 1.65 0 0 0-1.51 1z"></path>
-                              </svg>
-                              Settings
-                            </Link>
-                          </li>
-                          <li>
-                            <Link to="/">
-                              <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                width="24"
-                                height="24"
-                                viewBox="0 0 24 24"
-                                fill="none"
-                                stroke="currentColor"
-                                strokeWidth="2"
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                className="feather feather-volume-1"
-                              >
-                                <polygon points="11 5 6 9 2 9 2 15 6 15 11 19 11 5"></polygon>
-                                <path d="M15.54 8.46a5 5 0 0 1 0 7.07"></path>
-                              </svg>
-                              Logout
-                            </Link>
-                          </li>
-                        </ul>
-                      </div>
+                      <DASidebar />
                     </div>
                   </div>
                   <div className="col-xl-9 col-lg-9 col-md-12">
@@ -328,8 +255,9 @@ const AdminDashboard = () => {
                             <select
                               className="form-select"
                               aria-label="Default select example"
+                              defaultValue="HTML"
                             >
-                              <option selected>HTML</option>
+                              <option value="HTML">HTML</option>
                               <option value="1">CSS</option>
                               <option value="2">Javascript</option>
                               <option value="3">PHP</option>
@@ -449,9 +377,7 @@ const AdminDashboard = () => {
                               </div>
                               <div className="dashboard__popular__instructor__content">
                                 <h4>
-                                  <Link to="/instructor-details">
-                                    Sndi Jac
-                                  </Link>
+                                  <Link to="/instructor-details">Sndi Jac</Link>
                                 </h4>
                                 <ul>
                                   <li>
@@ -475,9 +401,7 @@ const AdminDashboard = () => {
                               </div>
                               <div className="dashboard__popular__instructor__content">
                                 <h4>
-                                  <Link to="/instructor-details">
-                                    Sndi Jac
-                                  </Link>
+                                  <Link to="/instructor-details">Sndi Jac</Link>
                                 </h4>
                                 <ul>
                                   <li>
@@ -529,12 +453,13 @@ const AdminDashboard = () => {
                                       </Link>
                                     </li>
                                     <li>
-                                      <i className="icofont-book-alt"></i> 9 Lesson
+                                      <i className="icofont-book-alt"></i> 9
+                                      Lesson
                                     </li>
 
                                     <li>
-                                      <i className="icofont-clock-time"></i> 3 hr 30
-                                      min
+                                      <i className="icofont-clock-time"></i> 3
+                                      hr 30 min
                                     </li>
                                   </ul>
                                 </div>
@@ -564,12 +489,13 @@ const AdminDashboard = () => {
                                       </Link>
                                     </li>
                                     <li>
-                                      <i className="icofont-book-alt"></i> 9 Lesson
+                                      <i className="icofont-book-alt"></i> 9
+                                      Lesson
                                     </li>
 
                                     <li>
-                                      <i className="icofont-clock-time"></i>2 hr 30
-                                      min
+                                      <i className="icofont-clock-time"></i>2 hr
+                                      30 min
                                     </li>
                                   </ul>
                                 </div>
@@ -598,11 +524,12 @@ const AdminDashboard = () => {
                                       </Link>
                                     </li>
                                     <li>
-                                      <i className="icofont-book-alt"></i> 5 Lesson
+                                      <i className="icofont-book-alt"></i> 5
+                                      Lesson
                                     </li>
                                     <li>
-                                      <i className="icofont-clock-time"></i> 1 hr 30
-                                      min
+                                      <i className="icofont-clock-time"></i> 1
+                                      hr 30 min
                                     </li>
                                   </ul>
                                 </div>
@@ -631,11 +558,12 @@ const AdminDashboard = () => {
                                       </Link>
                                     </li>
                                     <li>
-                                      <i className="icofont-book-alt"></i> 7 Lesson
+                                      <i className="icofont-book-alt"></i> 7
+                                      Lesson
                                     </li>
                                     <li>
-                                      <i className="icofont-clock-time"></i> 3 hr 30
-                                      min
+                                      <i className="icofont-clock-time"></i> 3
+                                      hr 30 min
                                     </li>
                                   </ul>
                                 </div>
